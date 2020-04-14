0e9fbe6b317b766171fb1cd7d1d5d59f2255723c
Mozer <43972902+mozerpol@users.noreply.github.com>
2020-04-14 16:19:12 +0200

    Revert "Replace variables with constants"
    This reverts commit 3d8a545b1ca1d33899c66a1a39694a2ad677ff96.

--- app.py
+++ app.py
@@ -12,14 +12,12 @@ bootstrap = Bootstrap(app)
 
 
 class NameForm(FlaskForm):
+    wrapsForm = IntegerField('Insert wraps: (5)', validators=[DataRequired()])
+    innerDiameterForm = FloatField('Insert inner diameter: (3.2)', validators=[DataRequired()])
+    wireDiameterForm = FloatField('Insert wire diameter: (0.322)', validators=[DataRequired()])
+    legsLengthForm = IntegerField('Insert legs length: (15)', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
-
-legsLength = 15
-innerDiameter = 3.2
-wireDiameter = 0.322
-outerDiameter = 0.322
-wraps = 5
 
 
 @app.errorhandler(404)
@@ -30,9 +28,43 @@ def page_not_found(e):
 def index():
     form = NameForm()
     if form.validate_on_submit():
+        session['wrapsForm'] = form.wrapsForm.data
+        session['innerDiameterForm'] = form.innerDiameterForm.data
+        session['wireDiameterForm'] = form.wireDiameterForm.data
+        session['legsLengthForm'] = form.legsLengthForm.data
+        if session['wrapsForm'] is None:
+            session['known'] = False
+        else:
+            session['known'] = True
+        
+        session['wrapsForm'] = form.wrapsForm.data
+        session['innerDiameterForm'] = form.innerDiameterForm.data
+        session['wireDiameterForm'] = form.wireDiameterForm.data
+        session['legsLengthForm'] = form.legsLengthForm.data
+        
         print(serialize_geom(
-            normalcoil(wraps, innerDiameter, wireDiameter, legsLength))
-            )
+            normalcoil(form.wrapsForm.data,
+						form.innerDiameterForm.data,
+						form.wireDiameterForm.data,
+						form.legsLengthForm.data
+					    )
+					)
+				)
+        """
+        session['normalcoilResult'] = normalcoil(form.wrapsForm.data,
+										form.innerDiameterForm.data,
+										form.wireDiameterForm.data,
+										form.legsLengthForm.data
+										)
+        """
         return redirect(url_for('index'))
-    return render_template( 'index.html',
-        form=form, known=session.get('known', False))
+    return render_template(
+            'index.html',
+            form=form,
+            wrapsForm=session.get('wrapsForm'),
+            innerDiameterForm=session.get('innerDiameterForm'), 
+            wireDiameterForm=session.get('wireDiameterForm'), 
+            legsLengthForm=session.get('legsLengthForm'), 
+            normalcoilResult=session.get('normalcoilResult'),
+            known=session.get('known', False)
+            )
