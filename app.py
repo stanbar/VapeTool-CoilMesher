1ab3e40a98a9824d0db6148eca9a5e909082ac32
Mozer <43972902+mozerpol@users.noreply.github.com>
2020-04-14 16:19:12 +0200

    Revert "Move html files to CLI and use curl, parse JSON"
    This reverts commit f2724405492923b48266945ad057c7ba5b9c5757.

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
-from flask import Flask, render_template, redirect, url_for, send_file, request
+from flask import Flask, render_template, redirect, url_for, send_file
+from flask_wtf import FlaskForm
+from wtforms import SubmitField
+from flask_bootstrap import Bootstrap
 from coil import normalcoil, serialize_geom, generate_mesh
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
+bootstrap = Bootstrap(app)
+
+
+class NameForm(FlaskForm):
+    submit = SubmitField('Submit')
 
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return "page not found\n"
+    return render_template('404.html')
 
 
 @app.route('/return-file/', methods=['GET'])
@@ -16,23 +24,14 @@ def returnFile():
     try:
         return send_file('coil.obj', attachment_filename='coil.obj')
     except:
-        return "error return-file\n"
-
-
-@app.route('/generate-mesh', methods=['GET', 'POST'])
-def generateMesh():
-    if not request.is_json:
-        return "false\n"
-    else:
-        wraps = request.json.get('wraps')
-        innerDiameter = request.json.get('innerDiameter')
-        wireDiameter = request.json.get('wireDiameter')
-        legsLength = request.json.get('legsLength')
-        
-        return "JSON ok\n"
+        return render_template('404.html')
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    #generate_mesh(normalcoil(5, 3.2, 0.322, 15))
-    return redirect(url_for('generateMesh'))
+    form = NameForm()
+    if form.validate_on_submit():
+        generate_mesh(normalcoil(5, 3.2, 0.322, 15))
+        return redirect(url_for('returnFile'))
+        
+    return render_template('index.html', form=form)
