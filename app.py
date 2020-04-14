44211423a009a668e83045f7120da87c9942b3fd
Mozer <43972902+mozerpol@users.noreply.github.com>
2020-04-14 16:19:12 +0200

    Revert "Add coil.obj to gitignore, add send_file function"
    This reverts commit 51a84dae980fb01cf2c52f2b87240bbdf3fba1fc.

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-from flask import Flask, render_template, redirect, url_for, send_file
+from flask import Flask, render_template, session, redirect, url_for
 from flask_wtf import FlaskForm
-from wtforms import SubmitField
+from wtforms import SubmitField, IntegerField, FloatField
+from wtforms.validators import DataRequired
 from flask_bootstrap import Bootstrap
-from coil import normalcoil, serialize_geom, generate_mesh
+from coil import normalcoil, serialize_geom
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['SECRET_KEY'] = 'jakis ciag znakow'
 bootstrap = Bootstrap(app)
 
 
@@ -14,24 +15,24 @@ class NameForm(FlaskForm):
     submit = SubmitField('Submit')
 
 
+legsLength = 15
+innerDiameter = 3.2
+wireDiameter = 0.322
+outerDiameter = 0.322
+wraps = 5
+
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html')
-
-
-@app.route('/return-file/', methods=['GET'])
-def returnFile():
-    try:
-        return send_file('coil.obj', attachment_filename='coil.obj')
-    except:
-        return render_template('404.html')
-
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
     if form.validate_on_submit():
-        generate_mesh(normalcoil(5, 3.2, 0.322, 15))
-        return redirect(url_for('returnFile'))
-        
-    return render_template('index.html', form=form)
+        print(serialize_geom(
+            normalcoil(wraps, innerDiameter, wireDiameter, legsLength))
+            )
+        return redirect(url_for('index'))
+    return render_template( 'index.html',
+        form=form, known=session.get('known', False))
