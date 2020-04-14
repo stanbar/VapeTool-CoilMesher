b87bf8c8dd414cf66258d9b32e78a15a0526f0f2
mozerpol <mozerpol@protonmail.com>
2020-04-14 14:35:00 +0200

    Move html files to CLI and use curl, parse JSON

--- app.py
+++ app.py
@@ -1,22 +1,14 @@
-from flask import Flask, render_template, redirect, url_for, send_file
-from flask_wtf import FlaskForm
-from wtforms import SubmitField
-from flask_bootstrap import Bootstrap
+from flask import Flask, render_template, redirect, url_for, send_file, request
 from coil import normalcoil, serialize_geom, generate_mesh
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
-bootstrap = Bootstrap(app)
-
-
-class NameForm(FlaskForm):
-    submit = SubmitField('Submit')
 
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html')
+    return "page not found\n"
 
 
 @app.route('/return-file/', methods=['GET'])
@@ -24,14 +16,23 @@ def returnFile():
     try:
         return send_file('coil.obj', attachment_filename='coil.obj')
     except:
-        return render_template('404.html')
+        return "error return-file\n"
+
+
+@app.route('/generate-mesh', methods=['GET', 'POST'])
+def generateMesh():
+    if not request.is_json:
+        return "false\n"
+    else:
+        wraps = request.json.get('wraps')
+        innerDiameter = request.json.get('innerDiameter')
+        wireDiameter = request.json.get('wireDiameter')
+        legsLength = request.json.get('legsLength')
+        
+        return "JSON ok\n"
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    form = NameForm()
-    if form.validate_on_submit():
-        generate_mesh(normalcoil(5, 3.2, 0.322, 15))
-        return redirect(url_for('returnFile'))
-        
-    return render_template('index.html', form=form)
+    #generate_mesh(normalcoil(5, 3.2, 0.322, 15))
+    return redirect(url_for('generateMesh'))
