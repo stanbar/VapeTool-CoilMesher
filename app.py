640a170e1eeeb24d3e7861429abc5d7c7d5f6703
mozerpol <mozerpol@protonmail.com>
2020-04-14 12:34:21 +0200

    Connect forms with coil.py

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask, render_template, session, redirect, url_for
 from flask_wtf import FlaskForm
-from wtforms import SubmitField
-from wtforms import IntegerField
+from wtforms import SubmitField, IntegerField, FloatField
 from wtforms.validators import DataRequired
 from flask_bootstrap import Bootstrap
-from coil import normalcoil
+from coil import normalcoil, serialize_geom
 
 
 app = Flask(__name__)
@@ -13,11 +12,12 @@ bootstrap = Bootstrap(app)
 
 
 class NameForm(FlaskForm):
-    wrapsForm = IntegerField('Insert wraps: ', validators=[DataRequired()])
-    innerDiameterForm = IntegerField('Insert inner diameter: ', validators=[DataRequired()])
-    wireDiameterForm = IntegerField('Insert wire diameter: ', validators=[DataRequired()])
-    legsLengthForm = IntegerField('Insert legs length: ', validators=[DataRequired()])
+    wrapsForm = IntegerField('Insert wraps: (5)', validators=[DataRequired()])
+    innerDiameterForm = FloatField('Insert inner diameter: (3.2)', validators=[DataRequired()])
+    wireDiameterForm = FloatField('Insert wire diameter: (0.322)', validators=[DataRequired()])
+    legsLengthForm = IntegerField('Insert legs length: (15)', validators=[DataRequired()])
     submit = SubmitField('Submit')
+
 
 
 @app.errorhandler(404)
@@ -41,6 +41,22 @@ def index():
         session['innerDiameterForm'] = form.innerDiameterForm.data
         session['wireDiameterForm'] = form.wireDiameterForm.data
         session['legsLengthForm'] = form.legsLengthForm.data
+        
+        print(serialize_geom(
+            normalcoil(form.wrapsForm.data,
+						form.innerDiameterForm.data,
+						form.wireDiameterForm.data,
+						form.legsLengthForm.data
+					    )
+					)
+				)
+        """
+        session['normalcoilResult'] = normalcoil(form.wrapsForm.data,
+										form.innerDiameterForm.data,
+										form.wireDiameterForm.data,
+										form.legsLengthForm.data
+										)
+        """
         return redirect(url_for('index'))
     return render_template(
             'index.html',
@@ -49,5 +65,6 @@ def index():
             innerDiameterForm=session.get('innerDiameterForm'), 
             wireDiameterForm=session.get('wireDiameterForm'), 
             legsLengthForm=session.get('legsLengthForm'), 
+            normalcoilResult=session.get('normalcoilResult'),
             known=session.get('known', False)
             )
